d3f009ac94318e981de9bdeab01112674a732d7c
DanPristupov <danil@danil.cz>
2014-08-26 12:01:45 +0200

    Adds CsvExtensions.ToCsvFile unit test

--- src/Orc.CsvHelper.Test/CsvExtensionsTest.cs
+++ src/Orc.CsvHelper.Test/CsvExtensionsTest.cs
@@ -8,6 +8,10 @@
 namespace Orc.CsvHelper.Test
 {
     using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
     using Csv;
     using Csv.Test.CsvMaps;
     using Csv.Test.Entities;
@@ -29,8 +33,100 @@ namespace Orc.CsvHelper.Test
             Assert.AreEqual(new TimeSpan(0,15,0), result[1].Duration);
             Assert.AreEqual(14.15, result[1].Quantity);
             Assert.AreEqual(true, result[1].Enabled);
+            var expectedResult = CreateSampleOperations();
+        [TestMethod]
+        public void ToCsvFile()
+        {
+            var file = @"Operation.csv";
+            var operations = CreateSampleOperations();
+            operations.ToCsv(file, typeof(OperationCsvMap));
+
+            var resultCsvLines = File.ReadAllLines(file) as IEnumerable<string>;
+
+            var expectedCsvLines = CreateSampleCsv();
+            AssertCollectionsAreEqual(expectedCsvLines, resultCsvLines);
+
+            // clean up
+            File.Delete(file);
+        }
+        #endregion
+
+
+        #region Helpers
+
+        private void AssertCollectionsAreEqual(IEnumerable<string> expectedCsvLines, IEnumerable<string> resultCsvLines)
+        {
+            var iterator = resultCsvLines.GetEnumerator();
+            foreach (var expectedCsvLine in expectedCsvLines)
+            {
+                iterator.MoveNext();
+                var resultCsvLine = iterator.Current;
+                Assert.AreEqual(expectedCsvLine, resultCsvLine);
+            }
         }
 
+        private IEnumerable<string> CreateSampleCsv()
+        {
+            yield return @"Id,Name,StartTime,Duration,Quantity,Enabled";
+            yield return @"0,Operation1,30/05/2014 7:30:00 PM,00:00:00,14,False";
+            yield return @"1,Operation2,30/05/2014 8:30:00 PM,00:15:00,14.15,True";
+            yield return @"2,Operation3,30/05/2014 9:30:00 PM,00:30:00,14.3,False";
+            yield return @"3,Operation4,30/05/2014 10:30:00 PM,00:45:00,14.45,True";
+            yield return @"4,Operation5,30/05/2014 10:30:00 PM,01:00:00,14.6,False";
+        }
+
+        private IEnumerable<Operation> CreateSampleOperations()
+        {
+            yield return new Operation
+            {
+                Id = 0,
+                Name = "Operation1",
+                StartTime = new DateTime(2014, 5, 30, 19, 30, 0),
+                Duration = new TimeSpan(0, 0, 0),
+                Quantity = 14.0,
+                Enabled = false,
+            };
+
+            yield return new Operation
+            {
+                Id = 1,
+                Name = "Operation2",
+                StartTime = new DateTime(2014, 5, 30, 20, 30, 0),
+                Duration = new TimeSpan(0, 15, 0),
+                Quantity = 14.15,
+                Enabled = true,
+            };
+
+            yield return new Operation
+            {
+                Id = 2,
+                Name = "Operation3",
+                StartTime = new DateTime(2014, 5, 30, 21, 30, 0),
+                Duration = new TimeSpan(0, 30, 0),
+                Quantity = 14.3,
+                Enabled = false,
+            };
+
+            yield return new Operation
+            {
+                Id = 3,
+                Name = "Operation4",
+                StartTime = new DateTime(2014, 5, 30, 22, 30, 0),
+                Duration = new TimeSpan(0, 45, 0),
+                Quantity = 14.45,
+                Enabled = true,
+            };
+
+            yield return new Operation
+            {
+                Id = 4,
+                Name = "Operation5",
+                StartTime = new DateTime(2014, 5, 30, 22, 30, 0),
+                Duration = new TimeSpan(1, 0, 0),
+                Quantity = 14.60,
+                Enabled = false,
+            };
+        }
         #endregion
     }
 }
