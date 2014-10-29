e0c1f556282f481cb11e4a8a25498d2aea8439fc
Geert van Horrik <geert@catenalogic.com>
2014-10-29 12:47:17 +0100

    Improved CsvHelper with exception handler

--- src/Orc.CsvHelper/Helpers/CsvReaderHelper.cs
+++ src/Orc.CsvHelper/Helpers/CsvReaderHelper.cs
@@ -7,27 +7,57 @@
 
 namespace Orc.Csv
 {
+    using System;
     using System.Collections.Generic;
     using System.IO;
-    using System.Linq;
+    using Catel.Logging;
     using CsvHelper;
     using CsvHelper.Configuration;
 
     public static class CsvReaderHelper
     {
+        private static readonly ILog Log = LogManager.GetCurrentClassLogger();
+
         #region Methods
-        public static IEnumerable<T> ReadCsv<T, TMap>(string csvFileName)
+        public static IEnumerable<T> ReadCsv<T, TMap>(string csvFileName, Action<T> initializer = null)
             where TMap : CsvClassMap
         {
+            if (!File.Exists(csvFileName))
+            {
+                throw new FileNotFoundException(string.Format("{0} doesn't exist", csvFileName));
+            }
+
+            var items = new List<T>();
+
             using (var streamReader = new StreamReader(csvFileName))
             {
                 using (var csvReader = new CsvReader(streamReader))
                 {
                     csvReader.Configuration.RegisterClassMap<TMap>();
 
-                    return csvReader.GetRecords<T>().ToList();
+                    try
+                    {
+                        while (csvReader.Read())
+                        {
+                            var record = csvReader.GetRecord<T>();
+                            if (initializer != null)
+                            {
+                                initializer(record);
+                            }
+
+                            items.Add(record);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        var errorMessage = ex.Data["CsvHelper"].ToString();
+
+                        Log.Warning("Cannot read row in '{0}'. Error Details: {1}", Path.GetFileName(csvFileName), errorMessage);
+                    }
                 }
             }
+
+            return items;
         }
 
         public static CsvReader CreateReader(string csvFileName)
