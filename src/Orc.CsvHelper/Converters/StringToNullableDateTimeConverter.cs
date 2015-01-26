924f9a3fa65eee5c00474b146419539099265c46
Dan Pristupov <danil@danil.cz>
2015-01-26 18:13:23 +0100

    Replaces static converter methods with separate classes.

---
+++ src/Orc.CsvHelper/Converters/StringToNullableDateTimeConverter.cs
@@ -0,0 +1,40 @@
+﻿namespace Orc.Csv
+{
+    using System;
+
+    public class StringToNullableDateTimeConverter : TypeConverter<DateTime?>
+    {
+        public StringToNullableDateTimeConverter()
+            : base(ConvertFromString)
+        {
+        }
+
+        public StringToNullableDateTimeConverter(string defaultInput)
+            : base(ConvertFromString, defaultInput)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static DateTime? ConvertFromString(string s1, string s2)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static DateTime? ConvertFromString(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return null;
+            }
+
+            var dateTime = Convert.ToDateTime(input);
+            if (dateTime == CsvEnvironment.ExcelNullDate)
+            {
+                return null;
+            }
+
+            return dateTime;
+        }
+
+    }
+}
---
+++ src/Orc.CsvHelper/Converters/YesNoToBooleanConverter.cs
@@ -0,0 +1,43 @@
+﻿namespace Orc.Csv
+{
+    using System;
+
+    public class YesNoToBooleanConverter : TypeConverter<bool>
+    {
+        public YesNoToBooleanConverter()
+            : base(ConvertFromString)
+        {
+        }
+
+        public YesNoToBooleanConverter(string defaultInput)
+            : base(ConvertFromString, defaultInput)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool ConvertFromString(string s1, string s2)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool ConvertFromString(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            input = input.ToLower();
+            if (input == "yes")
+            {
+                return true;
+            }
+            if (input == "no")
+            {
+                return false;
+            }
+            return false;
+        }
+
+    }
+}
