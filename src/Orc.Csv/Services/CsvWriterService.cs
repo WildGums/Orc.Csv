3f4148e801dd8ebd9dde73328cc5ccc235a0cf63
Maksim Khomutov <mkhomutov@gmail.com>
2019-03-12 14:44:45 +0400

    fix tests

--- src/Orc.Csv/Services/CsvWriterService.cs
+++ src/Orc.Csv/Services/CsvWriterService.cs
@@ -59,7 +59,7 @@ namespace Orc.Csv
                 Log.Debug($"Writing records");
 
                 var enumerator = records.GetEnumerator();
-                if (csvContext.Configuration.HasHeaderRecord && !enumerator.MoveNext())
+                if (csvContext.Configuration?.HasHeaderRecord??true && !enumerator.MoveNext())
                 {
                     // 0 records, only write header
                     csvWriter.WriteHeader(csvContext.RecordType);
