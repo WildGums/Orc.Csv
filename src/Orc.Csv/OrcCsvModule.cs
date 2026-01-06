bbbd9c52253a63c073d7e91491f356fcb397c32f
Geert van Horrik <geert@catenalogic.com>
2026-01-06 16:58:32 +0100

    Update

--- src/Orc.Csv/OrcCsvModule.cs
+++ src/Orc.Csv/OrcCsvModule.cs
@@ -1,9 +1,10 @@
-﻿namespace Orc.Csv
+﻿namespace Orc
 {
     using Catel.Services;
     using Catel.ThirdPartyNotices;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.DependencyInjection.Extensions;
+    using Orc.Csv;
 
     /// <summary>
     /// Core module which allows the registration of default services in the service collection.
