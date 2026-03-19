4d034b6ba471cb5a7c574cc8a194bc4ba6945421
Geert van Horrik <geert@catenalogic.com>
2026-03-19 14:31:22 +0100

    Update

--- src/Orc.Csv.Tests/Helpers/ServiceCollectionHelper.cs
+++ src/Orc.Csv.Tests/Helpers/ServiceCollectionHelper.cs
@@ -1,22 +1,19 @@
-﻿namespace Orc.Csv.Tests
+﻿namespace Orc.Csv.Tests;
+
+using Catel;
+using Microsoft.Extensions.DependencyInjection;
+
+internal static class ServiceCollectionHelper
 {
-    using Catel;
-    using Microsoft.Extensions.DependencyInjection;
-    using Orc.Csv;
-    using Orc.FileSystem;
-
-    internal static class ServiceCollectionHelper
+    public static IServiceCollection CreateServiceCollection()
     {
-        public static IServiceCollection CreateServiceCollection()
-        {
-            var serviceCollection = new ServiceCollection();
+        var serviceCollection = new ServiceCollection();
 
-            serviceCollection.AddLogging();
-            serviceCollection.AddCatelCore();
-            serviceCollection.AddOrcFileSystem();
-            serviceCollection.AddOrcCsv();
+        serviceCollection.AddLogging();
+        serviceCollection.AddCatelCore();
+        serviceCollection.AddOrcFileSystem();
+        serviceCollection.AddOrcCsv();
 
-            return serviceCollection;
-        }
+        return serviceCollection;
     }
 }
